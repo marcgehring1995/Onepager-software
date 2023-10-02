db943a5134395c9f35f862d4033a456c6328bb44
Enes Akgyun <eakgun161@gmail.com>
2023-10-02 12:28:28 +0200

    More parameters, markdown output, layout adjustment

--- testing.py
+++ testing.py
@@ -8,28 +8,37 @@ import tempfile
 from llama_index import Document
 from llama_index.query_engine.retriever_query_engine import RetrieverQueryEngine
 from llama_index.indices.vector_store.retrievers import VectorIndexRetriever
+import pyrebase
 load_dotenv()
-# Set up Llama Index
 
 # Set up Streamlit app
+st.set_page_config(layout='wide')
 st.title('One-Pager')
+input_column, response_column = st.columns([2,3])
+uploaded_file = input_column.file_uploader("Choose a PDF file", type="pdf")
 
-uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-
-formality = st.slider('Formality', 0, 100, 50)
+formality = input_column.slider('Formality', 0, 100, 50)
 
 # Add inputs for user context
-user = st.text_input('Who are you?')
-audience = st.text_input('Who is the summary for?')
+user = input_column.text_input('Who are you?')
+audience = input_column.text_input('Who is the summary for?')
+
+# Add inputs for user goal
+goal = input_column.text_input('What is your goal?')
 
 # Add dropdown for response structure
-structure = st.selectbox('Structure of the response', ['Summary', 'One-pager', 'Report', 'Speech', 'Presentation','Testament'])
+structure = input_column.selectbox('Structure of the response', ['Summary', 'One-pager', 'Report', 'Speech', 'Presentation','Testament'])
+
+# Add dropdown for summary length
+generation_length = input_column.selectbox('Generation Length', ['Short', 'Medium', 'Long'])
+
+language = input_column.selectbox('Language', ['English', 'Spanish', 'French', 'German', 'Italian'])
 
 # Add slider for temperature
-temperature = st.slider('Temperature', 0.0, 1.0, 0.5)
+temperature = input_column.slider('Temperature', 0.0, 1.0, 0.5)
 
 # Add slider for max tokens
-max_tokens = st.slider('Max Tokens', 100, 1000, 500)
+max_tokens = input_column.slider('Max Tokens', 100, 2000, 500)
 
 if uploaded_file is not None:
     # Create the ServiceContext with the user-selected temperature
@@ -41,7 +50,7 @@ if uploaded_file is not None:
         documents = [Document(text=text)]
         index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 
-    if st.button('Generate'):
+    if input_column.button('Generate'):
         # Determine formality phrase
         if formality < 33:
             formality_phrase = "In a casual and conversational style, "
@@ -51,7 +60,7 @@ if uploaded_file is not None:
             formality_phrase = "In a highly formal and academic style, "
 
         # Add user context and structure to the query
-        query = f"As {user}, I need a {structure.lower()} of the document for {audience}. {formality_phrase}Generate the {structure.lower()}."
+        query = f"As {user}, I need a {structure.lower()} of the document for {audience}. My goal is {goal}. I want a {generation_length.lower()} summary in {language}. Please provide the response in markdown format with appropriate features. {formality_phrase}Generate the {structure.lower()}."
 
         # Generate the response
         with st.spinner(f'Generating {structure.lower()}...'):
@@ -63,6 +72,6 @@ if uploaded_file is not None:
 
     # Display the response stored in the session state
     if 'response' in st.session_state:
-        st.write(st.session_state['response'])
+        response_column.markdown(st.session_state['response'])
 
-st.markdown("<p style='text-align: center;'> Brought to you with ❤ by WeConnectAI </p>", unsafe_allow_html=True)
+input_column.markdown("<p style='text-align: center;'> Brought to you with ❤ by WeConnectAI </p>", unsafe_allow_html=True)
